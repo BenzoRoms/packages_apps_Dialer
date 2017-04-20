3e7c00e5ca0a0ba92d5408bcd60bce2eb08fa76b
Srikanth Chintala <srikchin@codeaurora.org>
2017-04-20 16:05:43 +0530

    Display InCallUI on auto answer
    When a MT call is auto answered bring the InCallUI to
    foreground by starting the InCallActivity.
    
    Bug: 37529880
    Change-Id: I77e21f41699fa3ff78dd793d2f226ad95d11497f
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- InCallUI/src/com/android/incallui/InCallPresenter.java
+++ InCallUI/src/com/android/incallui/InCallPresenter.java
@@ -1310,6 +1310,17 @@ public class InCallPresenter implements CallList.Listener,
 
         // TODO: Consider a proper state machine implementation
 
+        //If the call is auto answered bring up the InCallActivity
+        boolean isAutoAnswer = false;
+
+        if ((mCallList.getDisconnectedCall() == null) &&
+                (mCallList.getDisconnectingCall() == null)) {
+            isAutoAnswer = (mInCallState == InCallState.INCOMING) &&
+                    (newState == InCallState.INCALL) &&
+                    (mInCallActivity == null);
+        }
+
+        Log.d(this, "startOrFinishUi: " + isAutoAnswer);
         // If the state isn't changing we have already done any starting/stopping of activities in
         // a previous pass...so lets cut out early
         if (newState == mInCallState) {
@@ -1386,7 +1397,7 @@ public class InCallPresenter implements CallList.Listener,
             return mInCallState;
         }
 
-        if (showCallUi || showAccountPicker) {
+        if (showCallUi || showAccountPicker || isAutoAnswer) {
             Log.i(this, "Start in call UI");
             showInCall(false /* showDialpad */, !showAccountPicker /* newOutgoingCall */);
         } else if (startIncomingCallSequence) {
